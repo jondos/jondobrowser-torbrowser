2c0025c156c01925f19efb68cd2121338ed6d828
Tinker <tinker@ubuntu.cardival.org>
2017-07-26 18:16:19 +0800

    #1633 icon, redirect link changed for changelog after update

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -17,7 +17,7 @@ pref("browser.usedOnWindows10", true);
 // each update manifest should contain attributes similar to:
 //   actions="showURL"
 //   openURL="https://blog.torproject.org/tor-browser-55a2-released"
-pref("startup.homepage_override_url", "https://blog.torproject.org/category/tags/tor-browser");
+pref("startup.homepage_override_url", "https://jondobrowser.jondos.de/blog/jondobrowser-released/");
 
 // Try to nag a bit more about updates: Pop up a restart dialog an hour after the initial dialog
 pref("app.update.promptWaitTime", 3600);
